84b479210966770f961afed54320c0d88d645c51
lebrancconvas <lebrancconvas@gmail.com>
2025-08-17 04:11:36 +0700

    ✨ feat: update main function to be using the working state machine.

--- index.ts
+++ index.ts
@@ -1,20 +1,10 @@
-import { State, Machine } from "./utils/state";
+import { workMachine } from "./examples";
 
 function main() {
-  const s0 = new State(0);
-  const s1 = new State(1);
-  const s2 = new State(2);
 
-  const stateMachine = new Machine(s0, s1, s2);
+  const workflows = ["load", "load", "enter", "restart", "complete"];
+  const result = workMachine.report(workflows);
 
-  stateMachine.link(s0, s1, "a");
-  stateMachine.link(s1, s2, "b");
-  stateMachine.link(s2, s0, "c");
-  stateMachine.link(s0, s0, "b");
-  stateMachine.link(s1, s1, "c");
-  stateMachine.link(s2, s2, "a");
-
-  let result = stateMachine.process("rinne");
   console.log(result);
 };
 
